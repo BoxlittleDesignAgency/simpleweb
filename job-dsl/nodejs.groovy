79d827b4abc25bf1d08d1f83b6fbee86c34e7343
Demola <ademoladebayo@gmail.com>
2021-07-19 17:16:48 +0100

    Editted job dsl

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -9,7 +9,7 @@ job('NodeJS app') {
      scm('H/5 * * * *')
    }
    wrappers {
-     Nodejs('nodejs') // this is the name of the NodeJS installation in
+     nodejs('nodejs') // this is the name of the NodeJS installation in
                       // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
    }
    steps {
